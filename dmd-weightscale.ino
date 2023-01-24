31ab1371a6cb06319610ee5058e6b5532f284cd6
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2023-01-24 14:41:33 +0200

    Update dmd-weightscale.ino

--- dmd-weightscale.ino
+++ dmd-weightscale.ino
@@ -57,7 +57,7 @@ DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
  *https://github.com/aguegu/ardulibs/tree/3cdb78f3727d9682f7fd22156604fc1e4edd75d1/hx711
  *adapted by Nicu FLORICA (niq_ro) for 200kg scale cell
  */
-#include <Hx711.h>
+#include <hx711.h>
 Hx711 scale(A2, A3);
 // Hx711.DOUT - pin #A2
 // Hx711.SCK - pin #A3
